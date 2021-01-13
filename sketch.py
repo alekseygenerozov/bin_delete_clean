e1528f4809a9a1b42e66c0f53875e12c4f475758
aleksey <aleksey.generozov@gmail.com>
2021-01-13 16:27:20 -0700

    Adding comments

--- sketch.py
+++ sketch.py
@@ -3,14 +3,17 @@ import rebound
 import bin_delete
 
 
+##N is the true number of stars you want in your simulation
 N=100
+##Add a buffer
 N2=int(1.5*N)
 
-##N is the true number of particles to delete
 ##add particles as usual...and initialize simulation as usual.
 #sim=...
+##Delete binaries
 bin_delete.delete_bins(sim)
 
+#Delete excess particles so we are left with N stars + (central object.)
 to_del=range(N+1, len(sim.particles))[::-1]
 for idx in to_del:
 	# print(type(idx))
